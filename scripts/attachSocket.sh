29264eeb82be05a15aea0e31b12436363f905069
Mark <themiddleman@gmail.com>
2019-04-12 00:51:41 -0700

    change flow

--- scripts/attachSocket.sh
+++ scripts/attachSocket.sh
@@ -3,13 +3,8 @@
 PORTSTART=3001
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-echo "WARNING: This program attaches intentionally vulnerable programs"
-echo "to sockets, making them accessible to other computers with access"
-echo "to your network"
-#cd ..
-
 declare -A programs
-for i in $(find /app/src/ | sort); do
+for i in $(find /app/bin/ | sort); do
 	FNAME=$(basename -- "${i%.*}")
 	if [[ "$FNAME" =~ "level" ]]; then
 		PORT=$PORTSTART
@@ -30,13 +25,19 @@ if [ -d "/app/www" ]; then
 	echo "<html><div><code>" > /app/www/index.html
 	echo "${THEGAME//$'\n'/<br />}" >> /app/www/index.html
 	echo "$programs <br />" >> /app/www/index.html
-        echo "<br /><b>Binaries can be found in /bin, source in /src.</b><br />" >> /app/www/index.html
-        echo "ASLR is enabled. Libc was recently loaded to ${LEAKER//$'\n'/<br />}" >> /app/www/index.html
+        echo "<br /><b>Binaries are in /bin, source in /src.</b><br />" >> /app/www/index.html
+        echo "ASLR enabled. Libc was recently loaded to ${LEAKER//$'\n'/<br />}" >> /app/www/index.html
 	echo "</code></div></html>" >> /app/www/index.html
 fi
 
 printf "Processes connected!\n"
+echo  "$THEGAME"
+echo "WARNING: This program attaches intentionally vulnerable programs"
+echo "to sockets, making them accessible to other computers with access"
+echo "to your network"
+
 while read -p "Press 'q' to quit, any other key to continue: " word; do
+	
 	if [[ "$word" == 'q' ]]; then
 		echo "Terminating child processes..."
 		break
